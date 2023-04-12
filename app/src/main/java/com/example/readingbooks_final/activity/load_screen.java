a55ed98d7328ad249767f27243846f33ad31af59
Ha Thi Thanh Thao <51900558@student.tdtu.edu.vn>
2023-04-12 22:03:52 +0700

    //Thanh Thảo update

---
+++ app/src/main/java/com/example/readingbooks_final/activity/load_screen.java
@@ -0,0 +1,16 @@
+package com.example.readingbooks_final.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readingbooks_final.R;
+
+public class load_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+    }
+}
---
+++ app/src/main/java/com/example/readingbooks_final/activity/profile.java
@@ -0,0 +1,16 @@
+package com.example.readingbooks_final.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readingbooks_final.R;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
